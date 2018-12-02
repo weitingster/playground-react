2da67b28d4484dce981c76175352f6275dc5e392
Weitingster <weitingxus@gmail.com>
2018-12-01 23:14:43 -0500

    added the faker files required to get fake data and created 3 comments replicates

--- components/src/index.js
+++ components/src/index.js
@@ -5,9 +5,10 @@ import faker from 'faker'; //This is from the faker js data set
 const App = () => {
   return (
     <div className="ui container comments">
+
       <div className="comment">
           <a href="/" className="avatar">
-            <img alt="avatar" src{faker.image.avatar()}/>
+            <img alt="avatar" src={faker.image.avatar()}/>
           </a>
           <div className="content">
             <a href="/" className="author">
@@ -21,6 +22,43 @@ const App = () => {
           </div>
         </div>
       </div>
+
+
+      <div className="comment">
+          <a href="/" className="avatar">
+            <img alt="avatar" src={faker.image.avatar()}/>
+          </a>
+          <div className="content">
+            <a href="/" className="author">
+              Sam
+            </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">
+          Nice blog post!
+          </div>
+        </div>
+      </div>
+
+
+      <div className="comment">
+          <a href="/" className="avatar">
+            <img alt="avatar" src={faker.image.avatar()}/>
+          </a>
+          <div className="content">
+            <a href="/" className="author">
+              Sam
+            </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">
+          Nice blog post!
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
