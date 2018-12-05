e418fb01fcdd277e0e4a4d7adcdf57e649c6e3da
Weiting <weiting@Weitings-MacBook-Pro.local>
2018-12-05 18:52:36 -0500

    added approve and reject buttons in the 3 comment cards

--- components/src/index.js
+++ components/src/index.js
@@ -2,31 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker'; //This is from the faker js data set
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:30PM"
+        actualComment="Wow, that's awesome"
+        avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
 
+      <ApprovalCard>
       <CommentDetail
-      author="Sam"
-      timeAgo="Today at 4:30PM"
-      actualComment="Wow, that's awesome"
-      avatar={faker.image.avatar()}
-      />
+        author="Jane"
+        timeAgo="Today at 2:00AM"
+        actualComment="Dude, sweet"
+        avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
 
+      <ApprovalCard>
       <CommentDetail
-      author="Jane"
-      timeAgo="Today at 2:00AM"
-      actualComment="Dude, sweet"
-      avatar={faker.image.avatar()}
-      />
-
-      <CommentDetail
-      author="Billy"
-      timeAgo="Yesterday at 2:39PM"
-      actualComment="Nailed it!"
-      avatar={faker.image.avatar()}
-      />
+        author="Billy"
+        timeAgo="Yesterday at 2:39PM"
+        actualComment="Nailed it!"
+        avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
 
     </div>
   );
