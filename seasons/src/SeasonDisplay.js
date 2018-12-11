753b837456ca956dec7a655a1ee89438c4b581ae
Weiting <weiting@Weitings-MacBook-Pro.local>
2018-12-10 21:12:55 -0500

    added two snowflake icons, removed constructor and super by replacing it with state, increased word size.

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+
+const SeasonDisplay = props => {
+  const season = getSeason (props.lat, new Date().getMonth());
+  const text = season === 'winter' ? 'Burr, it is chilly' : 'let us hit the beach!';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+
   return (
-    <div>yoyo</div>
-  );
+    <div>
+    <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+    <i className={`${icon} icon`} />
+    </div>
+);
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    // The constructor method will be called before anything else
-    // This is initializing the state
-    // Not always required to add "props" property
-  super(props);
-  // super is a reference to the parent's function React.Component
-  // must do every time when we initialize a class function
 
-  // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO this.state
-  this.state = { lat: null, errorMessage: '' };
-  // when we don't know what the number, i.e. what the latitude is, yet we will write null.
+  state = { lat: null, errorMessage: '' };
 
-  window.navigator.geolocation.getCurrentPosition(
-    position => {
-      // we called setState!
-      this.setState({ lat: position.coords.latitude });
-    },
-    err => {
-      this.setState({ errorMessage: err.message });
-    }
-  );
-}
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
 
   // React says we have to define render!!
   // Required for every single component
@@ -33,7 +21,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading!</div>;
