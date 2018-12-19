1a729f9dd40059e11ed4d6850ae841b3ca9a0800
Weiting <weiting@Weitings-MacBook-Pro.local>
2018-12-19 07:47:09 -0500

    refacted to the short form event e for onInputChange event handler

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchBar extends React.Component {
 
   onInputChange(event){
-    console.log(event.target.value)
+    console.log(event.target.value);
   }
 
   onInputClick() {
@@ -16,7 +16,7 @@ class SearchBar extends React.Component {
       <form className="ui form">
         <div className="field">
           <label>Image Search</label>
-          <input type="text" onChange={this.onInputChange} onClick={this.onInputClick}/>
+          <input type="text" onChange={(e) => console.log(e.target.value)} onClick={this.onInputClick}/>
         </div>
       </form>
     </div>
