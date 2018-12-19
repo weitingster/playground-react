04f0cfabe23e2b41d643513d171a9500d4fc6c65
Weiting <weiting@Weitings-MacBook-Pro.local>
2018-12-19 07:44:29 -0500

    added onChange and onClick eent handlers

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+  onInputChange(event){
+    console.log(event.target.value)
+  }
+
+  onInputClick() {
+    console.log('input was clicked');
+  }
+
   render() {
     return (
     <div className="ui segment">
       <form className="ui form">
         <div className="field">
           <label>Image Search</label>
-          <input type="text" />
+          <input type="text" onChange={this.onInputChange} onClick={this.onInputClick}/>
         </div>
       </form>
     </div>
