eb73d7c4fa4b464ac80dfc9f7f770758fc599818
Weiting <weiting@Weitings-MacBook-Pro.local>
2018-12-23 22:42:26 -0500

    added unsplash and retrieved api access key. note the network console is not behaving as expected yet. tbd

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,14 +3,24 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  state = { images: [] };
+
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID fa0215d5b42ccde9beb8737b7a88f6b287697768ac4227a1d248775815dce9bd'
+      }
+    });
+
+    this.setState ({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     );
   }
