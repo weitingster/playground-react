80f6e7d32f24a6843e301e521c13f42474b271d3
Weiting <weiting@Weitings-MacBook-Pro.local>
2018-12-23 22:14:23 -0500

    added axios package, and added it to app.js file

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px'}}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
+    );
+  }
+}
 
 export default App;
