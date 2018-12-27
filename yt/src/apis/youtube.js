dd774594764835a70cefe7e42a9ed2d7e5ac8c43
Weiting <weitingxus@gmail.com>
2018-12-26 20:35:04 -0500

    installed axios, added API paramters

--- yt/src/apis/youtube.js
+++ yt/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyDvLlFQO4Npo8TQvShyCa9y7FoYeLTkmHY';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+  }
+});
