cb79165e2b8def3d84bef8f9411c0d54dbbe561b
Weiting <weitingxus@gmail.com>
2018-12-26 21:28:38 -0500

    styled video item component

--- yt/src/components/VideoItem.js
+++ yt/src/components/VideoItem.js
@@ -1,12 +1,16 @@
+import './VideoItem.css';
 import React from 'react';
 
 const VideoItem = ({ video }) => {
   return (
-  <div>
-    <img src={video.snippet.thumbnails.medium.url} />
-    {video.snippet.title}
+  <div className="video-item item">
+    <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+        <div className="description">{video.snippet.description}</div>
+      </div>
   </div>
-);
+  );
 };
 
 export default VideoItem;
--- yt/src/components/VideoList.js
+++ yt/src/components/VideoList.js
@@ -6,7 +6,7 @@ const VideoList = ({ videos }) => {
       return <VideoItem video={video} />
     });
 
-  return <div>{renderedList}</div>;
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
