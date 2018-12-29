d4772d6b8abe786e8efcc614fb9fbc0e353524c3
Weiting <weitingxus@gmail.com>
2018-12-28 20:36:31 -0500

    alternatives to allow lodash to call our users just once and not repeated. learned memoize vs action creators in action creators

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
 
   render () {
     //const user = this.props.users.find((user) => user.id === this.props.userId );
@@ -27,4 +23,4 @@ const mapStateToProps = (state, ownProps) => {
   return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
