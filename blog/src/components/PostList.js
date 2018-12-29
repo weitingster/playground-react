4a9f5d7e8da5dcd962790d06705c3e6658f58b6a
Weiting <weitingxus@gmail.com>
2018-12-28 19:29:32 -0500

    pulled in json file to build a blog post app

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
 
   componentDidMount() {
     this.props.fetchPosts();
   }
-  
+
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+            <UserHeader userId={post.userId}/>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
-      <div>
-        Hello!
+      <div className="ui relaxed divided list">
+        {this.renderList()}
       </div>
     );
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
+
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
