626664369e28114175e20ccd9868dfe169a839f7
Weiting <weitingxus@gmail.com>
2018-12-28 22:46:03 -0500

    scaffold app with files create, delete, edit, list, and show

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'; //Had an npm install
-
-const PageOne = () => {
-  return (
-    <div>PageOne
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-  <div> PageTwo
-    <button>Click Me</button>
-    <Link to="/">Navigate to Page One</Link>
-  </div>
-  );
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
+      <h1>Header</h1>
+
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
