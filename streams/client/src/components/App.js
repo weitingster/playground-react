60723b5f0f38bfe5a74dd8242b1cc3fda698b741
Weiting <weitingxus@gmail.com>
2018-12-28 22:19:27 -0500

    starting new app to replicate Twitch, modified

---
+++ streams/client/src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; //Had an npm install
+
+const PageOne = () => {
+  return (
+    <div>PageOne
+      <Link to="/pagetwo">Navigate to Page Two</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+  <div> PageTwo
+    <button>Click Me</button>
+    <Link to="/">Navigate to Page One</Link>
+  </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
