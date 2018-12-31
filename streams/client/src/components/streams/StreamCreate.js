db8e63be7b9f8ce47b4b59ddff3cd1f5bf35c8c7
Weiting <weitingxus@gmail.com>
2018-12-31 15:27:50 -0500

    made the stream creation form with title and description, and submit button; app doesn't allow user to type in title and description yet, defect

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return (
-    <div>StreamCreate</div>
-  );
+class StreamCreate extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header"> {error} </div>
+        </div>
+      );
+    }
+  }
+
+//this produced an error message related to property so we changed it to the arrow function
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error': ''}`;
+
+    return (
+      <div className={className}>
+      <label>{label}</label>
+      <input {...input} />
+      {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+      onSubmit={this.props.handleSubmit(this.onSubmit)}
+      className="ui form error"
+      >
+        <Field
+        name="title"
+        component={ this.renderInput }
+        label="Enter Title"
+        />
+        <Field
+        name="description"
+        component={ this.renderInput }
+        label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+//handles error messages when user didn't type in an appropriate title or description
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = 'Please type in a title';
+  }
+
+  if (!formValues.description) {
+    errors.description = "Please enter a description";
+  }
+
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate', //can be any casing
+  validate
+})(StreamCreate);
