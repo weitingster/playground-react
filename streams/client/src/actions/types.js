0ae0609e5d04371026f7fec2722ae60c2c1e553d
Weiting <weitingxus@gmail.com>
2018-12-31 16:40:47 -0500

    made all of our action creators

--- streams/client/src/actions/types.js
+++ streams/client/src/actions/types.js
@@ -1,3 +1,7 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
 export const CREATE_STREAM = 'CREATE_STREAM';
+export const FETCH_STREAMS = 'FETCH_STREAMS'; // multiple streams
+export const FETCH_STREAM = 'FETCH_STREAM'; // single stream
+export const DELETE_STREAM = 'DELETE STREAM';
+export const EDIT_STREAM = 'EDIT_STREAM';
