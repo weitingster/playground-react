4843c28069765486887f5a1354007e83b6547e87
Weiting <weitingxus@gmail.com>
2019-01-01 16:13:20 -0500

    created a context STORE to connect the selector

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../context/LanguageContext';
+import { LanguageStore } from '../context/LanguageContext';
 import ColorContext from '../context/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
-  state = { language: 'english' }
-
-  onLanguageChange = language => {
-    this.setState({ language });
-  };
-
     render() {
       return (
         <div className="ui container">
-          <div>
-            <h2>Select a language</h2>
-            <i className="flag us" onClick={() => this.onLanguageChange('english')}  />
-            <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
-          </div>
+        <LanguageStore>
+          <LanguageSelector />
 
-          <ColorContext.Provider value="primary">
-            <LanguageContext.Provider value={this.state.language}>
-              <UserCreate />
-            </LanguageContext.Provider>
+            <ColorContext.Provider value="primary">
+            <UserCreate />
           </ColorContext.Provider>
+        </LanguageStore>
         </div>
       );
     }
--- translate/src/context/LanguageContext.js
+++ translate/src/context/LanguageContext.js
@@ -1,3 +1,21 @@
 import React from 'react';
 
-export default React.createContext('dutch');
+const Context = React.createContext('english');
+
+export class LanguageStore extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  }
+
+  render() {
+    return (
+      <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
