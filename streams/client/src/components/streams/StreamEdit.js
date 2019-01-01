4c656f833add18645fbbbbb411b30b37d5d29ae4
Weiting <weitingxus@gmail.com>
2018-12-31 22:35:52 -0500

    refactored the StreamCreate to StreamCreation

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
-  return (
-    <div>StreamEdit</div>
-  );
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    //First refresh of the stream edit page will return undefined in ID parameter
+    //Therefore, we put a loading first
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+  return <div>{this.props.stream.title}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id]};
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
