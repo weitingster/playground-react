057b2d8205e8ea9a9ba24bab08d185f47c567b86
Weiting <weitingxus@gmail.com>
2019-01-01 00:13:09 -0500

    refactored the modal so it can be reused

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
 const Modal = props => {
   return ReactDOM.createPortal(
     //The onClick allows us to click in the grey area to get rid of the modal
     <div
-    onClick={() => history.push('/')}
+    onClick={props.onDismiss}
     className="ui dimmer modals visible active"
     >
       //Modal for delete stream with onClick event does not bubble up to the
       //action where you click the modal and it reacts like CANCEL button
       <div onClick={(e) => { e.stopPropagation()}} className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
-        <div className="content">Are you sure you want to delete this stream?</div>
-        <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
-        </div>
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector('#modal') //rendered directly into index.html in body
--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
+
+  const actions = (
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
+
   return (
     <div>
-    <Modal /> StreamDelete
+    StreamDelete
+    <Modal
+      title="Delete Stream"
+      content="Are you sure you want to delete this stream?"
+      actions={actions}
+      onDismiss={() => history.push('/')}
+    />
     </div>
   );
 };
