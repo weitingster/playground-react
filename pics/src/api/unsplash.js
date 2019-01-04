4fece0a00972ad475f5581632def267187e6bd98
Weiting <weitingxus@gmail.com>
2019-01-03 19:58:29 -0500

    general removal

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID fa0215d5b42ccde9beb8737b7a88f6b287697768ac4227a1d248775815dce9bd'
+    Authorization: 'Client-ID insert-your-client-id-key'
   }
 });
--- yt/src/apis/youtube.js
+++ yt/src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyDvLlFQO4Npo8TQvShyCa9y7FoYeLTkmHY';
+const KEY = 'insert-your-KEY-here';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
